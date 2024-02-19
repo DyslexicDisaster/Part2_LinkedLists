e84fa2bd22196c831f870f8805d12ebe4ff0d5c6
smath <smathewslynch@gmail.com>
2024-02-19 16:18:53 +0000

    Created the Node class in the LinkedList Class and set up the head, tail and numElements

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -5,5 +5,34 @@ package utils;
  * @author michelle
  */
 public class LinkedList {
-    
+    private Node head;
+    private Node tail;
+    private int numElements;
+
+    protected static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            next = null;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return this.data;
+        }
+    }
 }
+
