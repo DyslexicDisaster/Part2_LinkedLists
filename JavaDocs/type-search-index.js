8e8cf3439172920e43d262b89006c11849747d9a
smath <smathewslynch@gmail.com>
2024-02-19 16:00:54 +0000

    Initial commit, Created song class with getters/Setters, equals, hashcode and toString methods

---
+++ JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"utils","l":"LinkedList"},{"p":"utils","l":"LinkedListExtraTests"},{"p":"utils","l":"LinkedListTest"},{"p":"business","l":"Song"}];updateSearchResults();
