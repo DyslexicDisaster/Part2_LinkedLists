cab2fe3c1960504dc939ee98a988c83e71daded6
smath <smathewslynch@gmail.com>
2024-03-04 19:14:24 +0000

    Made the add and indexOf Methods

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -37,6 +37,49 @@ public class LinkedList {
         return current.getData();
     }
 
+    public int indexOf(Song toBeFound){
+        Node current = head;
+        int count = 0;
+        while(current != null){
+            if (current.data.equals(toBeFound)){
+                return count;
+            }
+            count++;
+            current = current.getNext();
+        }
+        return -1;
+    }
+
+    /*public void addToStart(Song song) {
+        Node newNode = new Node(song);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        numElements++;
+    }*/
+
+    public void add(Song toBeAdded) {
+        if (toBeAdded == null) {
+            throw new IllegalArgumentException("Wrong stop that");
+        }
+
+        Node newNode = new Node(toBeAdded);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        numElements++;
+    }
+
     protected static class Node {
         private Song data;
         private Node next;
